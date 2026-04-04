f6f9859e8e70311dc6d750fb718a4ad42563904f
BARATAM NIDHISHRI <153882344+nidhibaratam@users.noreply.github.com>
2026-04-04 21:41:02 +0530

    Delete uvm_sample_module directory

--- uvm_sample_module/fifo_uvm_pkg.sv
+++
@@ -1,4 +0,0 @@
-package fifo_uvm_pkg;
-    // We include the classes inside the package scope
-    `include "tb_classes.sv"
-endpackage
--- uvm_sample_module/run.sh
+++
@@ -1,8 +0,0 @@
-# 1. Build the simulation
-rm -rf obj_dir
-rm -rf waveform.vcd
-verilator --binary -j 0 --timing --trace -Wno-WIDTHTRUNC --top-module top fifo_uvm_pkg.sv fifo_if.sv fifo.sv top.sv
-
-
-# 2. RUN the simulation
-./obj_dir/Vtop
